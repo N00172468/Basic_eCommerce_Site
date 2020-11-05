c3bcb24d2feaf05a6c3a3f477ab99de81ade5171
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2020-11-05 17:16:02 +0000

    Major Update:
    - Added DOM Manipulation:
    	- Set the appropriate background colour of leftCanvas if user
    	either enters "For Men"/"For Women" pages.
    - Created a function to fetch data from JSON file.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,71 @@
 $(document).ready(function() {
   console.log("Testing, 123");
+
+  /**
+   * REQUEST SECTION
+   * 
+   * @ref - https://www.codegrepper.com/code-examples/delphi/read+json+file+with+vanilla+javascript
+   * 
+   * - Getting data from JSON File.
+   * - Count as AJAX?
+   */
+  $.getJSON("watches.json", function(json) {
+    console.log('Objects', json); // This will show the info it in firebug console
+  
+    // If #men then run this:
+    json.mens.forEach(item => {
+      $('#men').append(
+        `<hr class="divider" />
+
+        <div class="row">
+          <img src="${item.image}" width="175"/>
+          <p class="contents">
+            ${item.name} 
+            <br/> 
+            ${item.manufacturer}
+            <br />
+            €${item.price}
+          </p>
+        </div>
+        
+        <hr class="divider" />`
+      );
+    });
+
+    // If #women then run this:
+    json.women.forEach(item => {
+      $('#women').append(
+        `<hr class="divider" />
+
+        <div class="row">
+          <img src="${item.image}" width="175"/>
+          <p class="contents">
+            ${item.name} 
+            <br/> 
+            ${item.manufacturer}
+            <br />
+            €${item.price}
+          </p>
+        </div>
+
+        <hr class="divider" />`
+      );
+    });
+
+    // Styles:
+    $('.contents').css('margin', 'auto');
+    $('.contents').css('color', 'black');
+    $('.contents').css('font-family', 'Poppins');
+    $('.contents').css('font-size', '20px');
+  });
+
+/**
+ * DOM MANIPULATION
+ * 
+ * Changing the left canvas background colour depending if it's the "For Him"/"For Her" section.
+ */
+$('#men').css("background-color", "#839deb");
+$('#women').css("background-color", "#e73895");
 });
+
+
