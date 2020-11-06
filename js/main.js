d9c8c65137bc5308e0f93e154dcd57e6e42743a1
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2020-11-06 16:51:26 +0000

    Created a Function that adds item to cart modal:
    - Same function to Selecting Elements.
    - Work in Progress.

--- js/main.js
+++ js/main.js
@@ -41,8 +41,11 @@ $(document).ready(function() {
                 View
             </button>
             
-            <button class="btn btn-outline-dark loginBtn" type="button">
-              Add to Cart
+            <button 
+              class="btn btn-outline-dark loginBtn" 
+              type="button"
+              onclick="fillEmptyCart(${JSON.stringify(item).replace(/"/g, '\'')})">
+                Add to Cart
             </button>
           </p>
         </div>
@@ -84,7 +87,10 @@ $(document).ready(function() {
                 View
             </button>
             
-            <button class="btn btn-outline-dark loginBtn" type="button">
+            <button 
+              class="btn btn-outline-dark loginBtn" 
+              type="button"
+              onclick="fillEmptyCart(${JSON.stringify(item).replace(/"/g, '\'')})">
               Add to Cart
             </button>
           </p>
@@ -141,8 +147,11 @@ function fillEmptyCanvas(item){
         €${item.price}
   
         <br/>
-        <button class="btn btn-outline-dark loginBtn" type="button">
-          Add to Cart
+        <button 
+          class="btn btn-outline-dark loginBtn" 
+          type="button"
+          onclick="fillEmptyCart(${JSON.stringify(item).replace(/"/g, '\'')})">
+            Add to Cart
         </button>
       </p>
       </div>
@@ -154,36 +163,62 @@ function fillEmptyCanvas(item){
 /**
  * CART MODAL
  */
-$("#cart").html(`
-  <button type="button" class="btn btn-outline-dark loginBtn" data-toggle="modal" data-target="#exampleModal">
-    <a class="rightNav" href="#" id="cart">
-      Cart 
-      <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-bag-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" d="M8 1a2.5 2.5 0 0 0-2.5 2.5V4h5v-.5A2.5 2.5 0 0 0 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5v9a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V5H2z"/>
-        <path fill-rule="evenodd" d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"/>
-      </svg>
-    </a>
-  </button>
-  
-  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          ...
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
+function fillEmptyCart(item){
+  $("#cart").html(`
+    <button type="button" class="btn btn-outline-dark loginBtn" data-toggle="modal" data-target="#exampleModal">
+      <a class="rightNav" href="#" id="cart">
+        Cart 
+        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-bag-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" d="M8 1a2.5 2.5 0 0 0-2.5 2.5V4h5v-.5A2.5 2.5 0 0 0 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5v9a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V5H2z"/>
+          <path fill-rule="evenodd" d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"/>
+        </svg>
+      </a>
+    </button>
+    
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Cart</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+
+            <div class="row emptyCanvas">
+              <div class="col">
+                <img src="${item.image}" width="200"/>
+              </div>
+              
+              <div class="col emptyCanvas viewContent">
+                <p>
+                <span class="darkGold">${item.name}</span> 
+          
+                <br/> 
+                ${item.manufacturer}
+          
+                <br />
+                €${item.price}
+          
+                <br/>
+                <button class="btn btn-outline-danger loginBtn" type="button">
+                  Remove
+                </button>
+              </p>
+              </div>
+            </div>
+
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Checkout</button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-`);
+  `);
+}
+
 
 
