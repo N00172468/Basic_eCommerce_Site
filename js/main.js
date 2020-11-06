b616cb3b1241453ee5ce31d0e419b325ec811956
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2020-11-06 21:55:53 +0000

    Added Function to Update Cart Badge:
    - Created an If/Else statement to update badge counter.
    - Work in Progress.

--- js/main.js
+++ js/main.js
@@ -167,13 +167,16 @@ function fillEmptyCart(item) {
   console.log('Object added to Cart: ', $("div", "#cartContent"));
 
   $("#cart").html(`
-    <button type="button" class="btn btn-outline-dark loginBtn" data-toggle="modal" data-target="#exampleModal">
+    <button type="button" class="btn btn-outline-dark cartBtn loginBtn" data-toggle="modal" data-target="#exampleModal">
       <a class="rightNav" href="#" id="cart">
-        Cart 
-        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-bag-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" d="M8 1a2.5 2.5 0 0 0-2.5 2.5V4h5v-.5A2.5 2.5 0 0 0 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5v9a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V5H2z"/>
-          <path fill-rule="evenodd" d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"/>
-        </svg>
+        <span class="badge cartBadge active">1</span>
+        <span class="btnTitle">Item Added</span> 
+        <span>
+          <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-bag-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M8 1a2.5 2.5 0 0 0-2.5 2.5V4h5v-.5A2.5 2.5 0 0 0 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5v9a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V5H2z"/>
+            <path fill-rule="evenodd" d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"/>
+          </svg>
+        </span>
       </a>
     </button>
     
@@ -233,7 +236,41 @@ function deleteContent() {
   $("#cartContent").remove();
 }
 
+/**
+ * UPDATING BADGE VALUE
+ * 
+ * @ref - https://stackoverflow.com/questions/28501175/updating-text-for-a-badge-inside-a-bootstrap-button
+ */
+$(document).on("click", ".cartBtn", function(e) {
+  e.preventDefault();
 
+  // var $label = $(this).find('.text'),
+  //   $badge = $(this).find('.badge'),
+  //   count = Number($badge.text()),
+  //   active = $(this).hasClass('active');
+    
+  // $label.text(active ? 'Item Added' : 'Cart Empty');
+  // $badge.text(active ? count - 1 : count + 1);
+  // $(this).toggleClass('active');
+
+  var btnTitle = $(this).find(".btnTitle");
+  var badge = $(this).find(".cartBadge");
+  var count = parseInt(badge.text());
+
+  if($(this).hasClass('active')) {
+    // $(document).on("click", ".removeBtn", function(e) {
+    //   e.preventDefault();
+      
+      btnTitle.text("Empty");
+      badge.text(count - 1);
+      $(this).removeClass('active');
+    // })
+  } else {
+    btnTitle.text("Item Added");
+    badge.text(count + 1);
+    $(this).addClass('active');
+  }
+});
 
 
 
